a90607cd28252aa546a045c055949021ad54e785
Ahadu Kebede <ahadukebede@gmail.com>
2025-05-02 22:13:28 -0400

    created react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import {useState} from 'react';
+import logo from './assets/images/logo-universal.png';
+import './App.css';
+import {Greet} from "../wailsjs/go/main/App";
+
+function App() {
+    const [resultText, setResultText] = useState("Please enter your name below 👇");
+    const [name, setName] = useState('');
+    const updateName = (e: any) => setName(e.target.value);
+    const updateResultText = (result: string) => setResultText(result);
+
+    function greet() {
+        Greet(name).then(updateResultText);
+    }
+
+    return (
+        <div id="App">
+
+        </div>
+    )
+}
+
+export default App
